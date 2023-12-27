acb1a8e30b451a267f617a32948ab89b47956e47
Abhishek Choudhary <abhishek.choudhary@cogoport.com>
2023-12-27 12:23:37 +0530

    database bug fixed

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config({ paht: "./env" });
+
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
-const connectDB = require(__dirname + "/db.js");
+const port = process.env.PORT || 3000;
+const connectDB = require("./db/db.js");
 
 app.use(express.json());
 connectDB();
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.get("/", (req,res) => {
-    res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use("/api", require("../Routes/CreateUser"));
-app.use("/api", require("./Routes/DisplayData"));
-app.use("/api", require("./Routes/OrderData"));
+app.use("/api", require("./Routes/CreateUser.js"));
+app.use("/api", require("./Routes/DisplayData.js"));
+app.use("/api", require("./Routes/OrderData.js"));
 
 app.listen(port, () => {
-    console.log(`server is started on port ${port}`);
-})
+  console.log(`server is started on port ${port}`);
+});
