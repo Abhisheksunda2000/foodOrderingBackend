af1811c4a77eded4e365798ad53a5a70559c7fce
Abhishek Choudhary <110471724+Abhisheksunda2000@users.noreply.github.com>
2024-01-04 20:09:01 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import {app} from "./app.js";
-// import connectDB from "./db/db.js";
+import connectDB from "./db/db.js";
 dotenv.config({ path: "./.env" });
 
 
