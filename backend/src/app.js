fbfd9362c4ecc60391ff38ed72605048df9b2850
Abhishek Choudhary <110471724+Abhisheksunda2000@users.noreply.github.com>
2024-01-05 01:18:03 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,22 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
+const allowedOrigins = process.env.CORS_ORIGIN  ? process.env.CORS_ORIGIN.split(',')  : ['http://localhost:3000', 'http://localhost:3001'];
 // configurations
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials:true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true, 
+  })
+);
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
